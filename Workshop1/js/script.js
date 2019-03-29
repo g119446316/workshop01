8cf951ee991df563249b48813e30854e74120bc5
eorge G <likemyself12345@gmail.com>
2019-03-29 15:14:43 +0800

    新增功能，但是新增不能按第二次

--- Workshop1/js/script.js
+++ Workshop1/js/script.js
@@ -17,6 +17,108 @@ function loadBookData() {
     }
 }
 
+function book_gird() {
+    $("#book_grid").kendoGrid({
+        dataSource: {
+            type: "json",
+            data: bookData,
+            pageSize: 20,
+        },
+        height: 550,
+        pageable: true,
+        columns:
+            [
+                { command: { text: "刪除", click: DeleteButtonIsClicked } },
+                {
+                    field: "BookId",
+                    title: "書籍編號",
+                    width: 80,
+                },
+                {
+                    field: "BookName",
+                    title: "書籍名稱",
+
+                    width: 200,
+                },
+                {
+                    field: "BookCategory",
+                    title: "書籍種類",
+                    width: 100,
+                },
+
+                {
+                    field: "BookAuthor",
+                    title: "作者",
+                    width: 130,
+                },
+                {
+                    field: "BookBoughtDate",
+                    title: "購買日期",
+                    format: "{0:yyyy-MM-dd}",
+                    width: 100
+                },
+                {
+                    field: "a",
+                    title: "送達狀態",
+                    width: 100,
+                },
+                {
+                    field: "BookPrice",
+                    title: "金額",
+                    width: 100,
+                    format: "{0:0,0}"
+                }, {
+                    field: "BookAmount",
+                    title: "數量",
+                    width: 100,
+
+                    //format: "{0:MM/dd/yyyy}"
+                },
+                {
+                    field: "BookTotal",
+                    title: "總計",
+                    width: 100,
+                    format: "{0:0,00元}"
+
+                }],
+
+    });
+}
+
+function DeleteButtonIsClicked(e) {
+    var tr = $(e.target).closest("tr"); // get the current table row (tr)
+    var data = this.dataItem(tr);
+    var dataSource = $("#book_grid").data("kendoGrid").dataSource;
+    console.log("Details for: " + data.BookId);
+    kendo.confirm("確定刪除「" + data.BookName + "」 嗎?").then(function () {
+        dataSource.remove(data);
+    });
+}
+
+function form() {
+$("#add_book").click(function () {   
+    $("#ad").kendoWindow({
+        title: "新增書籍",
+        actions: ["pin", "Minimize", "Maximize", "Close"],
+        modal: true,
+        width: "500px",
+        height: "600px",        
+        position: {
+            top: 100,
+            left: "35%"
+        },
+        close:onClose
+
+    });
+    
+   
+});
+}
+
+
 $(function () {
+    book_gird();
     loadBookData();
+    form();
+    
 });
